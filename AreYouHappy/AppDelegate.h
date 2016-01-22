1b26a4cc7011bed5c15ff05df6ae6f1f9e2d4461
Sarosh Salman Mirza <mirza.salman.sarosh@gmail.com>
2016-01-23 00:08:47 +0100

    project files

---
+++ AreYouHappy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AreYouHappy
+//
+//  Created by Sarosh Salman Mirza on 23/01/16.
+//  Copyright (c) 2016 mycodenights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AreYouHappy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AreYouHappy
+//
+//  Created by Sarosh Salman Mirza on 23/01/16.
+//  Copyright (c) 2016 mycodenights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
